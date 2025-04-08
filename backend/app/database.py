2654a464cd3321f8e413b5aa3a5c4379aaf29384
Vijay Patha <elderpatha@gmail.com>
2025-04-08 14:35:29 -0600

    database.py update

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,10 +2,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import make_url
 from app.config import DATABASE_URL
 
 Base = declarative_base()
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+url = make_url(DATABASE_URL)
+connect_args = {"check_same_thread": False} if url.drivername.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
